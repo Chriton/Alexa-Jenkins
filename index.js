bd625de4588a97c63209acb8d0c55c76957abca8
Doru Muntean <munteandoru@gmail.com>
2017-04-25 23:56:41 +0300

    some code cleaning

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  *
  * https://github.com/bignerdranch/developing-alexa-skills-solutions
  */
+
 'use strict';
 module.change_code = 1;
 var _ = require('lodash');
@@ -11,36 +12,37 @@ var skill = new Alexa.app('jenkins');
 var JenkinsHelper = require('./jenkins_helper');
 
 skill.launch(function(req, res) {
-	var prompt = 'Tell me the name of the job you want to start.';
+	let prompt = "Tell me the name of the job you want to start.";
 	res.say(prompt).reprompt(prompt).shouldEndSession(false);
 });
 
-
+/**
+ * Start a build in Jenkins
+ * If a build/job name is NOT provided, Alexa will ask for one
+ */
 skill.intent('buildJenkinsJobIntent', {
 		'slots': {
-			// 'Jobname': 'JOBS'
 			'Jobname': 'AMAZON.LITERAL'
 		},
 		'utterances': ["{|build|deploy|start|run} {-|Jobname}", "{|build|deploy|start|run}", "{-|Jobname}"]
 	},
 	function(req, res) {
-		var job = req.slot('Jobname');
-		var reprompt = 'Tell me the name of the job you would like me to start.';
+		let job = req.slot('Jobname');
+		let reprompt = "Tell me the name of the job you would like me to start.";
 
 		if (_.isEmpty(job)) {
-			var prompt = 'I didn\'t hear the job name. Tell me the name of the job you want to start.';
+			let prompt = "I didn't hear the job name. Tell me the name of the job you want to start.";
 			res.say(prompt).reprompt(reprompt).shouldEndSession(false);
 			return true;
 		} else {
-			var jenkinsBuildJob = new JenkinsHelper();
-			console.log(job);
+			let jenkinsBuildJob = new JenkinsHelper();
+			//console.log(job);
 			jenkinsBuildJob.buildJob(job).then(function(result) {
-				console.log(result);
+				//console.log(result);
 				res.say(jenkinsBuildJob.buildResponse(result)).send();
 			}).catch(function(err) {
-				console.log(err.statusCode);
-				var prompt = 'I\'m sorry. I could\'t start the job. Jenkins responded with error code ' + err.statusCode; // 'The error message is: ' + err.message;
-				// res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+				//console.log(err.statusCode);
+				let prompt = "I'm sorry. I could't start the job. Jenkins responded with error code " + err.statusCode;
 				res.say(prompt).shouldEndSession(true).send();
 			});
 			return false;
@@ -48,27 +50,26 @@ skill.intent('buildJenkinsJobIntent', {
 	}
 );
 
-
+/**
+ * Respond with the number of jobs present in Jenkins
+ */
 skill.intent('listJenkinsJobsIntent', {
 	'slots': {},
 	'utterances': ["{|how many} {|jobs} {|are}"]
 },
 	function(req, res) {
-		var number_of_jobs = new JenkinsHelper();
+		let number_of_jobs = new JenkinsHelper();
 
 		number_of_jobs.listJobs().then(function(result) {
-			console.log(result);
-			// res.say(number_of_jobs.listResponse(result)).send();
+			//console.log(result);
 			res.say(number_of_jobs.listResponse(result)).shouldEndSession(true).send();
 		}).catch(function(err) {
-			console.log(err.statusCode);
-			var prompt = 'I\'m sorry. Jenkins responded with error code ' + err.statusCode;
+			//console.log(err.statusCode);
+			let prompt = "I'm sorry. Jenkins responded with error code " + err.statusCode;
 			res.say(prompt).shouldEndSession(true).send();
 		});
 		return false;
 	}
 );
-
-
 
 module.exports = skill;
--- jenkins_helper.js
+++ jenkins_helper.js
@@ -1,5 +1,6 @@
 /**
  * Created by doru.muntean on 23/04/17.
+ *
  */
 
 
@@ -7,6 +8,7 @@
 var _ = require('lodash');
 var requestPromise = require('request-promise');
 var http_success = [ 200, 201 ];
+
 
 /**
  * Your Jenkins address goes here
@@ -16,15 +18,16 @@ var ENDPOINT = 'http://79.115.45.174:8080';
 
 function JenkinsHelper() {}
 
+/**
+ * Send a request to the Jenkins API to start a job
+ * @param jobName - the name of the job you want to build
+ * @returns {*}
+ */
 JenkinsHelper.prototype.buildJob = function(jobName) {
+	let encodeJobName = encodeURI(jobName);
 
-	var encodeJobName = encodeURI(jobName);
-
-	var options = {
+	let options = {
 		method: 'POST',
-		// hostname: ENDPOINT,
-		// port: 8080,
-		// path: "/job/" + encodeJobName + "/build",
 		uri: ENDPOINT + "/job/" + encodeJobName + "/build",
 		//json: true,
 		resolveWithFullResponse: true,
@@ -32,50 +35,58 @@ JenkinsHelper.prototype.buildJob = function(jobName) {
 	return requestPromise(options);
 };
 
+/**
+ * Format the response from the buildJob function
+ * @param statusObject
+ * @returns {*}
+ */
+JenkinsHelper.prototype.buildResponse = function(statusObject) {
+    if (http_success.indexOf(statusObject.statusCode) > -1) {
+        let template = _.template("Ok, I've started the job. ");
+        return template({
+            status: statusObject.statusCode,
+        });
+    } else {
+        //status code received from Jenkins is not one of the http_success
+        let template =_.template("Sorry, I could't start the job. I've received the status code ${status} from jenkins");
+        return template({
+            status: statusObject.statusCode,
+        });
+    }
+};
 
+/**
+ * Request the number of jobs from the Jenkins API
+ * @returns {*}
+ */
 JenkinsHelper.prototype.listJobs = function() {
 
-	var options = {
+	let options = {
 		method: 'GET',
 		uri: ENDPOINT + "/api/json",
 		json: true,
-		// uri: "http://79.115.45.174:8080/api/json",
 		resolveWithFullResponse: true,
 	};
 	return requestPromise(options);
 };
 
+/**
+ * Extract and format the number of Jenkins jobs received from the listJobs function
+ * @param statusObject - the json response from the listJobs function
+ * @returns {*}
+ */
 JenkinsHelper.prototype.listResponse = function(statusObject) {
-	console.log(statusObject);
-	if (http_success.indexOf(statusObject.statusCode) > -1){
-		//count the number of jobs from the response
-		let template = _.template('There are ${number_of_jobs} jobs in jenkins.');
+	//console.log(statusObject);
+	if (http_success.indexOf(statusObject.statusCode) > -1) {
+		let template = _.template("There are ${number_of_jobs} jobs in jenkins.");
 		return template({
 			number_of_jobs: statusObject.body.jobs.length,
 		});
-	}
-	else {
-		//status code received from Jenkins is not one of the http_success
-		let template =_.template('I\'m sorry. I could\'t get that information. I\'ve received the status code ${status} from jenkins');
-		return template({
-			status: statusObject.statusCode,
-		});
-	}
-};
-
-
-JenkinsHelper.prototype.buildResponse = function(statusObject) {
-	if (http_success.indexOf(statusObject.statusCode) > -1){
-	// if(statusObject.statusCode === 201) {
-		var template = _.template('Ok, I\'ve started the job. '); //I\'ve received the status code ${status} from jenkins');
-		return template({
-		status: statusObject.statusCode,
-	});
 	} else {
 		//status code received from Jenkins is not one of the http_success
-		var template =_.template('Sorry, I could\'t start the job. I\'ve received the status code ${status} from jenkins');
+		let template =_.template("I'm sorry. I could't get that information. I've received the status code ${status} from jenkins");
 		return template({
-		status: statusObject.statusCode,
+			status: statusObject.statusCode,
 		});
 	}
 };
